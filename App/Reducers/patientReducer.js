f221e79c8c91cf12a6d02eede1ed596686de64f3
Markus Petrykowski <github@markus-petrykowski.de>
2016-11-27 09:39:16 +0200

    adding journey data

--- App/Reducers/patientReducer.js
+++ App/Reducers/patientReducer.js
@@ -18,7 +18,63 @@ const treatment = (state = "Knee Surgery", action) => {
   }
 }
 
+const handleWaypoint = (waypoint={}, action) => {
+  switch(action.type){
+    case 'CHECK_WAYPOINT':
+      return {
+        ...waypoint,
+        executed: waypoint.id == action.payload.id ? !waypoint.executed : waypoint.executed
+      }
+    default:
+      return {
+        ...waypoint
+      }
+  }
+}
+
+const journey = (state=journeyData, action) => {
+  switch(action.type){
+    case 'ADD_WAYPOINT':
+      return [
+        ...state,
+        action.payload.waypoint
+      ]
+    case 'CHECK_WAYPOINT':
+      return state.map( (waypoint) => (handleWaypoint(waypoint, action) ))
+    default:
+      return [...state]
+  }
+}
+
 export default combineReducers({
   id,
   treatment
 })
+
+
+let journeyData = [
+  {
+    id: 1,
+    section: 'Preparation',
+    title: 'Get Ready',
+    description: 'First of all we need to do something important',
+    executed: false,
+    image: 'background1',
+  },
+  {
+    id: 1,
+    section: 'Preparation',
+    title: 'Suitability',
+    description: 'In order to go to the mission we have to be fit enough for it',
+    executed: false,
+    image: 'background2',
+  },
+  {
+    id: 1,
+    section: 'Mission',
+    title: 'Sleeping Well',
+    description: 'To start our mission we first need to talk to Dr. XY.',
+    executed: false,
+    image: 'background3',
+  }
+]
